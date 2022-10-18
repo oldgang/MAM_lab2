fea3f0d46f7c55055da0901ee7ffb80646758cc5
oldgang <iceman16999@gmail.com>
2022-10-18 20:44:27 +0200

    Implemented storage permissions.

--- app/src/main/java/com/example/mam_lab2/MainActivity.java
+++ app/src/main/java/com/example/mam_lab2/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.mam_lab2;
 
+import static android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     Button playButton;
     Button stopButton;
+    boolean externalStorage = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(!checkStoragePermission()){
+            requestStoragePermission();
+        }
+    }
+
+    public boolean checkStoragePermission(){
+        if(Environment.isExternalStorageManager())
+            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+        return Environment.isExternalStorageManager();
+    }
+
+    public void requestStoragePermission(){
+        Intent intent = new Intent(ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                Uri.parse("package:" + BuildConfig.APPLICATION_ID));
+        startActivityForResult(intent, 999);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData)
+    {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (resultCode == RESULT_OK && requestCode == 999)
+        {
+            checkStoragePermission();
+        }
     }
 
     public void playMusic(){
